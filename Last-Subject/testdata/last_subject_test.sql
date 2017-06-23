5f3826f3a79db59e7611d332e8366c0f9e670ba1
watanabe.toshikazu <watanabe.toshikazu@watanabe>
2017-06-23 16:30:11 +0900

    Signed-off-by: watanabe.toshikazu <watanabe.toshikazu@watanabe>

--- Last-Subject/testdata/last_subject_test.sql
+++ Last-Subject/testdata/last_subject_test.sql
@@ -1,3 +1,7 @@
+drop database last_subject;
+create database last_subject;
+
+
 -- アカウント
 INSERT INTO ACCOUNT (email, password, username, id) VALUES ('watanabe@comnic.co.jp', 'watanabe', '渡邉要一', '1');
 INSERT INTO ACCOUNT (email, password, username, id) VALUES ('ishiduka@comnic.co.jp', 'ishiduka', '石塚北斗', '2');
@@ -11,11 +15,9 @@ INSERT INTO DEPARTMENT (id, name) VALUES ('3', '開発部');
 
 
 -- スケジュール
-INSERT INTO SCHEDULE (email, id, creatingday, work_category_id, starttime, endtime, memo) VALUES ('watanabe@comnic.co.jp', '1', '2017/06/23', '1', '', '', '');
+INSERT INTO SCHEDULE (email, id, creatingday, work_category_id, starttime, endtime, memo) VALUES ('watanabe@comnic.co.jp', '1', '2017-06-23', '1', '2017-06-23-00:00', '2017-06-23-00:10', '');
 
 
 -- 仕事カテゴリー
 INSERT INTO WORK_CATEGORY (id, name) VALUES ('1', '会議');
 INSERT INTO WORK_CATEGORY (id, name) VALUES ('2', '出張');
-
-
